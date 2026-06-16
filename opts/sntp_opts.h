0f55af7fec7dbf0f979d63e95b90503ca041f6b7
ToshihiroOgino <tonigo0725@gmail.com>
2026-06-16 18:49:05 +0900

    Update CMakeLists.txt to set board type to pico2_w and remove unused RTC library includes in sntp_opts.h

--- opts/sntp_opts.h
+++ opts/sntp_opts.h
@@ -2,7 +2,7 @@
 #define SNTP_OPTS_H
 
 #include <time.h>
-#include "hardware/rtc.h"
+// #include "hardware/rtc.h"
 #include "pico/util/datetime.h"
 
 #define SNTP_SUPPORT_MULTIPLE_SERVERS 1
@@ -14,18 +14,19 @@
 #define SNTP_DEBUG 1
 
 void sntp_sync_rtc(uint32_t sec) {
-    time_t raw_time = (time_t)sec;
-    struct tm *t = gmtime(&raw_time);
-    datetime_t dt = {
-        .year  = (int16_t)(t->tm_year + 1900),
-        .month = (int8_t)(t->tm_mon + 1),
-        .day   = (int8_t)t->tm_mday,
-        .dotw  = (int8_t)t->tm_wday, // 0=Sunday
-        .hour  = (int8_t)t->tm_hour,
-        .min   = (int8_t)t->tm_min,
-        .sec   = (int8_t)t->tm_sec
-    };
-    rtc_set_datetime(&dt);
+    // no op right now
+    // time_t raw_time = (time_t)sec;
+    // struct tm *t = gmtime(&raw_time);
+    // datetime_t dt = {
+    //     .year  = (int16_t)(t->tm_year + 1900),
+    //     .month = (int8_t)(t->tm_mon + 1),
+    //     .day   = (int8_t)t->tm_mday,
+    //     .dotw  = (int8_t)t->tm_wday, // 0=Sunday
+    //     .hour  = (int8_t)t->tm_hour,
+    //     .min   = (int8_t)t->tm_min,
+    //     .sec   = (int8_t)t->tm_sec
+    // };
+    // rtc_set_datetime(&dt);
 }
 
 #define SNTP_SET_SYSTEM_TIME(sec) sntp_sync_rtc(sec)
